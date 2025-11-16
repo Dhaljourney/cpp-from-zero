ea599fa8e9a054717ea231e67e3e099b7d87a9fd
afdhaljourney <25afdhalazhari@gmail.com>
2025-11-16 16:57:49 +0700

    Add files via upload

---
+++ hallo world.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    cout << "I'm Afdhal - welcome to my C++ learning journey!" << endl;
+    return 0;
+}
+
